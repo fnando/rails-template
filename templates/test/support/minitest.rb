30185e544e8dfb78445dd1c32a86db2febe781ce
Nando Vieira <fnando.vieira@gmail.com>
2017-09-09 00:30:18 -0700

    Add dependencies for running Capybara with headless Chrome.

--- templates/test/support/minitest.rb
+++ templates/test/support/minitest.rb
@@ -1,5 +1,5 @@
 require "minitest/utils"
 require "minitest/utils/rails"
 require "minitest/utils/rails/locale"
-
-require "minitest/autorun"
+require "minitest/utils/capybara/screenshot_on_failures"
+require "minitest/utils/capybara/chrome_headless"
