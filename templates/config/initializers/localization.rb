c3eb49ad0912ce447e119b3e770f0ddb797b3ad5
Nando Vieira <fnando.vieira@gmail.com>
2018-09-14 19:21:51 -0700

    Raise exception for missing translations.

--- templates/config/initializers/localization.rb
+++ templates/config/initializers/localization.rb
@@ -2,3 +2,7 @@ Rails.env.on(:any) do
   config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.yml")]
   config.time_zone = <%= app_const %>::Config.tz
 end
+
+Rails.env.on(:development, :test) do
+  config.action_view.raise_on_missing_translations = true
+end
